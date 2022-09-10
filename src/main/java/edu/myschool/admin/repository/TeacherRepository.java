fb721e0511b4863a06085e04334ff88a9b5d0ab9
Ishan Liyanage <ishanul@gmail.com>
2022-09-10 17:54:14 +0800

    Student and Teacher repositories

---
+++ src/main/java/edu/myschool/admin/repository/TeacherRepository.java
@@ -0,0 +1,10 @@
+package edu.myschool.admin.repository;
+
+import edu.myschool.admin.model.Teacher;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends CrudRepository<Teacher, Long> {
+
+}
